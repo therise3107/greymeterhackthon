ae36f153ba3b76445cbcd8111f08d482705b78a8
therise3107 <lyadav3107@gmail.com>
2015-06-14 05:28:29 +0530

    change to logic

--- app/controllers/logic_controller.rb
+++ app/controllers/logic_controller.rb
@@ -1,6 +1,10 @@
 class LogicController < ApplicationController
   @@total = []
   @@my_move = []
+  @@y = []
+  @@o = []
+
+
   def ping
     
     @@total = []
@@ -18,12 +22,18 @@ class LogicController < ApplicationController
       end
     end
     
+    
+  	@temp = params[:y].split(/\D/)
 
-  	@@y = [params[:y][0].to_i,params[:y][2].to_i]
+  	@@y = [@temp[0].to_i, @temp[1].to_i]
   	@@total.delete(@@y)
+  	
 
-  	@@o = [params[:o][0].to_i,params[:o][2].to_i]
+  	@tempo = params[:o].split(/\D/)
+  	@@o = [@tempo[0].to_i, @tempo[1].to_i]
+  	self.class.class_variable_get(:@@total).delete(@@o)
 
+  	
   	@@my_move = @@y # my intial move
 
   	render json: '{"ok": true}' 
@@ -31,7 +41,7 @@ class LogicController < ApplicationController
   end
 
   def play
-  	@available_moves = []
+  	#@available_moves = []
     @current_position = []
 	@y_adj = []
   	@o_adj = []
@@ -40,48 +50,52 @@ class LogicController < ApplicationController
 
 
   	@m = [params[:m][0].to_i, params[:m][2].to_i]   #move of oponent
-    self.class.class_variable_get(:@@total).delete(@m)                             #update the total moves
+    
 
-    self.class.class_variable_get(:@@total).delete(@@o)
+    
 
   	(@m[0]-1).upto(@m[0]+1) do |i|
   		(@m[1]-1).upto(@m[1]+1) do |j|
-  			if @@total.include?([i,j])
-              @m_adj << [i,j]	# opponent neighbours
+  			if self.class.class_variable_get(:@@total).include?([i,j])
+              @m_adj << [i,j]		# opponent neighbours
             end
         end
   	end
-    
+
+  	
 
   	(@@my_move[0]-1).upto(@@my_move[0]+1) do |i|
   		(@@my_move[1]-1).upto(@@my_move[1]+1) do |j|
-        if @@total.include?([i,j])
-          @o_adj << [i,j]# my neighbours
-        end
+        	if self.class.class_variable_get(:@@total).include?([i,j])
+          	  @o_adj << [i,j]				# my neighbours
+        	end
   		end
   	end
 
   	
-  	@available_moves = @o_adj
+  	#@available_moves = @o_adj
 
 
-  	if @o_adj.include?([@m])
-  		@current_position = @m
-  		
-      
+  	if @o_adj.include?(@m)
+  		@current_position = @m		
+  			   
     else
   		@m_adj.length.times do |i|
   			if @o_adj.include? @m_adj[i]
-  				@available_moves.delete(@m_adj[i])
+  				@o_adj.delete(@m_adj[i])
   			end
   		end
-      @current_position = @available_moves[rand(@available_moves.length)]
+   
+    @current_position = @o_adj[rand(@o_adj.length)]
+    
     end
 
 
-
-  	@@total.delete(@current_position)
-
+    @@my_move = @current_position
+    
+  	self.class.class_variable_get(:@@total).delete(@m)
+  	self.class.class_variable_get(:@@total).delete(@current_position)
+  	
 
   	
   	render json: {m: "#{@current_position[0]}|#{@current_position[1]}"}
